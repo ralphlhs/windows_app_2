fe1cb261921cd3afc09a5772f1b7ae6888e2554b
ralphlhs <ralphlhs@empas.com>
2022-07-27 02:13:30 +0900

    SignIn page 완성

--- lib/root.dart
+++ lib/root.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:windows_app_2/tab_page.dart';
 
+import 'loginPage.dart';
+
 class RootPage extends StatelessWidget {
   const RootPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const TabPage(title: 'Hi');
+    return const LogInIn();
   }
 }
