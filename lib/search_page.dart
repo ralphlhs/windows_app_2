c7709e6cf8cdf2a8c35a9faba0d8bc6c843a3615
ralphlhs <ralphlhs@empas.com>
2022-08-19 02:16:43 +0900

    Storage, Firestore과 연동 성공

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:windows_app_2/create_page.dart';
@@ -24,14 +25,16 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.user!.displayName!, style: const TextStyle(color: Colors.black)),
+        title: Text(widget.user!.displayName!,
+            style: const TextStyle(color: Colors.black)),
         backgroundColor: Colors.white,
       ),
       body: _buildBody(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push( context,
-          MaterialPageRoute(builder: (context) => CreatePage(user: widget.user!)),
+          Navigator.push(context,
+            MaterialPageRoute(
+                builder: (context) => CreatePage(user: widget.user!)),
           );
         },
         // _incrementCounter,
@@ -43,83 +46,91 @@ class _SearchPageState extends State<SearchPage> {
   }
 
   Widget _buildBody() {
-    return GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 5,
-            crossAxisCount: 3,
-            childAspectRatio: 1.0),
-        primary: false,
-        padding: const EdgeInsets.all(8),
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => Picview(
-                        height: "e.height",
-                        imageurl:
-                            "https://64.media.tumblr.com/88b38f715767d34d7a9d33ca93182e80/tumblr_of3e2uI1471vitnh0o4_400.jpg")),
-              );
-            },
-            child: Container(
-              padding: const EdgeInsets.all(2),
-              color: Colors.teal[100],
-              child: Image.network(
-                  "https://techbigs.com/uploads/2020/02/adorable-home-5717.jpg",
-                  fit: BoxFit.cover),
-            ),
-          );
-        }
-
-        // children: <Widget>[
-        //   InkWell(
-        //     onTap: () {
-        //       Navigator.push(
-        //         context,
-        //         MaterialPageRoute(
-        //             builder: (context) => Picview(
-        //                 height: "e.height",
-        //                 imageurl:
-        //                     "https://techbigs.com/uploads/2020/02/adorable-home-5717.jpg")),
-        //       );
-        //     },
-        //     child: Container(
-        //       padding: const EdgeInsets.all(8),
-        //       color: Colors.teal[100],
-        //       child: Image.network(
-        //           "https://techbigs.com/uploads/2020/02/adorable-home-5717.jpg"),
-        //     ),
-        //   ),
-        //   Container(
-        //     padding: const EdgeInsets.all(8),
-        //     color: Colors.teal[200],
-        //     child: Image.network(
-        //         "https://dbdzm869oupei.cloudfront.net/img/sticker/preview/29027.png"),
-        //   ),
-        //   Container(
-        //     padding: const EdgeInsets.all(8),
-        //     color: Colors.teal[300],
-        //     child: const Text('Sound of screams but the'),
-        //   ),
-        //   Container(
-        //     padding: const EdgeInsets.all(8),
-        //     color: Colors.teal[400],
-        //     child: const Text('Who scream'),
-        //   ),
-        //   Container(
-        //     padding: const EdgeInsets.all(8),
-        //     color: Colors.teal[500],
-        //     child: const Text('Revolution is coming...'),
-        //   ),
-        //   Container(
-        //     padding: const EdgeInsets.all(8),
-        //     color: Colors.teal[600],
-        //     child: const Text('Revolution, they...'),
-        //   ),
-        // ],
-        );
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('post').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        return Image.network(widget.user!.photoURL!);
+      },
+    );
   }
-}
+
+
+  // GridView.builder(
+  //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+  //       crossAxisSpacing: 5,
+  //       mainAxisSpacing: 5,
+  //       crossAxisCount: 3,
+  //       childAspectRatio: 1.0),
+  //   primary: false,
+  //   padding: const EdgeInsets.all(8),
+  //   itemCount: 10,
+  //   itemBuilder: (context, index) {
+  //     return InkWell(
+  //       onTap: () {
+  //         Navigator.push(
+  //           context,
+  //           MaterialPageRoute(
+  //               builder: (context) => Picview(
+  //                   height: "e.height",
+  //                   imageurl:
+  //                       "https://64.media.tumblr.com/88b38f715767d34d7a9d33ca93182e80/tumblr_of3e2uI1471vitnh0o4_400.jpg")),
+  //         );
+  //       },
+  //       child: Container(
+  //         padding: const EdgeInsets.all(2),
+  //         color: Colors.teal[100],
+  //         child: Image.network(
+  //             "https://techbigs.com/uploads/2020/02/adorable-home-5717.jpg",
+  //             fit: BoxFit.cover),
+  //       ),
+  //     );
+  //   }
+
+  // children: <Widget>[
+  //   InkWell(
+  //     onTap: () {
+  //       Navigator.push(
+  //         context,
+  //         MaterialPageRoute(
+  //             builder: (context) => Picview(
+  //                 height: "e.height",
+  //                 imageurl:
+  //                     "https://techbigs.com/uploads/2020/02/adorable-home-5717.jpg")),
+  //       );
+  //     },
+  //     child: Container(
+  //       padding: const EdgeInsets.all(8),
+  //       color: Colors.teal[100],
+  //       child: Image.network(
+  //           "https://techbigs.com/uploads/2020/02/adorable-home-5717.jpg"),
+  //     ),
+  //   ),
+  //   Container(
+  //     padding: const EdgeInsets.all(8),
+  //     color: Colors.teal[200],
+  //     child: Image.network(
+  //         "https://dbdzm869oupei.cloudfront.net/img/sticker/preview/29027.png"),
+  //   ),
+  //   Container(
+  //     padding: const EdgeInsets.all(8),
+  //     color: Colors.teal[300],
+  //     child: const Text('Sound of screams but the'),
+  //   ),
+  //   Container(
+  //     padding: const EdgeInsets.all(8),
+  //     color: Colors.teal[400],
+  //     child: const Text('Who scream'),
+  //   ),
+  //   Container(
+  //     padding: const EdgeInsets.all(8),
+  //     color: Colors.teal[500],
+  //     child: const Text('Revolution is coming...'),
+  //   ),
+  //   Container(
+  //     padding: const EdgeInsets.all(8),
+  //     color: Colors.teal[600],
+  //     child: const Text('Revolution, they...'),
+  //   ),
+  // ],
+  );
+}}
